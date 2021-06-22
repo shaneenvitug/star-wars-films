9a830d41e8d7f0c612a5d008123299eee5a45151
shaneen <shaneen@elephantroom.com.au>
2021-06-22 17:07:42 +1000

    Added basic search function to show movie titles

--- pages/api/films.js
+++ pages/api/films.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const response = await fetch(`https://swapi.dev/api/`)
+    const response = await fetch(`https://swapi.dev/api/films`)
     const data = await response.json()
 
     res.status(200).json(data)
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 function Home(props) {
+
+  const [searchInputValue, setSearchInputValue] = useState('');
   console.log(props)
   return (
     <div className={styles.container}>
-    Star Wars Films coming soon!
+      Star Wars Films coming soon!
+      <label>Search</label>
+      <input type="text" value={searchInputValue} onChange={(e) => setSearchInputValue(e.target.value)} />
+      {searchInputValue && props.data.results.filter(result => result.title.includes(searchInputValue)).map(result => <p key={result.episode_id}>{result.title}</p>)}
     </div>
   )
 }
