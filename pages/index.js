d9ab7c0e4b5c7149907b8ab7f58349252ab1143b
shaneen <shaneen@elephantroom.com.au>
2021-06-23 09:08:13 +1000

    Toggle add favourite on click

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,43 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 function Home(props) {
+  const starWarsFilms = props.data.results.map(result => ({...result, isFavourite: false}))
+  const [searchInputValue, setSearchInputValue] = useState('')
+  const [films, setFilms] = useState(starWarsFilms)
 
-  const [searchInputValue, setSearchInputValue] = useState('');
-  console.log(props)
+  const toggleFavourite = (id) => {
+    console.log(id)
+    setFilms((prevState) => {
+      // This was the fix below babs
+      // The reference to the old array instead of spreading the old array into a new array
+      const updatedFilms = [...prevState]
+      const filmIndex = updatedFilms.findIndex(film => film.episode_id === id)
+      updatedFilms[filmIndex] = { ...updatedFilms[filmIndex], isFavourite: !updatedFilms[filmIndex].isFavourite }
+      console.log("updatedFilms", updatedFilms)
+      return updatedFilms
+    })
+  }
+
+  console.log("films", films)
+
   return (
     <div className={styles.container}>
       Star Wars Films coming soon!
       <label>Search</label>
       <input type="text" value={searchInputValue} onChange={(e) => setSearchInputValue(e.target.value)} />
-      {searchInputValue && props.data.results.filter(result => result.title.includes(searchInputValue)).map(result => <p key={result.episode_id}>{result.title}</p>)}
+      {films.filter(result => result.title.toLowerCase()
+        .includes(searchInputValue))
+        .map(result => {
+          console.log("result.isFavourite", result.isFavourite)
+          return (
+            <p key={result.episode_id}>{result.title}
+            <span onClick={() => toggleFavourite(result.episode_id)}>
+              {console.log(`result.title is ${result.title}, and result.isFavourite is ${result.isFavourite}`)}
+              {result.isFavourite ? 'Favourite' : 'Not Favourite'}
+            </span>
+          </p>)}
+          )
+        }
     </div>
   )
 }
